daecbd8405dd916025424fcb9c8284a456f42003
Diego <diegosantosmtd@gmail.com>
2024-03-22 08:56:22 +0800

    👌 IMPROVE:

--- src/app/Technology.tsx
+++ src/app/Technology.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Flex, Text, Image, Box } from "@chakra-ui/react"
+import { Flex, Text, Image, Box, useMediaQuery } from "@chakra-ui/react"
 
 const Technology = () => {
+    const [isLowerThan768] = useMediaQuery("(max-width: 768px, max-height:800px)")
+
     return (
         <Flex
             id="Technologies"
@@ -29,68 +31,127 @@ const Technology = () => {
                     Below, I've outlined some of the cutting-edge technologies I'm proficient in as a front-end
                     developer.
                 </Text>
-                <Flex justifyContent="space-between" overflow="hidden" gap={{ base: "2", lg: "5" }} overflowX="auto">
-                    <Box minW="90px" minH="100px">
-                        <Image
-                            objectFit="contain"
-                            w="60px"
-                            h="60px"
-                            src="icons/Technology/react.svg"
-                            alt="React_Developers"
-                        />
+                <Flex
+                    justifyContent="space-between"
+                    overflow="hidden"
+                    gap={{ base: "2", lg: "5" }}
+                    overflowX="auto"
+                    align={"center"}
+                    paddingX={"20px"}>
+                    <Box
+                        minW="100px"
+                        minH="100px"
+                        transition="all .2s ease-in-out"
+                        cursor={"pointer"}
+                        _hover={{
+                            transform: !isLowerThan768 ? "scale(1.2)" : "",
+                        }}
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center">
+                        <Image w="70px" h="70px" src="icons/Technology/react.svg" alt="ReactJS" />
                     </Box>
-                    <Box minW="90px" minH="100px">
+
+                    <Box
+                        transition="all .2s ease-in-out"
+                        cursor={"pointer"}
+                        _hover={{
+                            transform: !isLowerThan768 ? "scale(1.2)" : "",
+                        }}
+                        minW="100px"
+                        minH="100px"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center">
+                        <Image objectFit="contain" w="90px" h="90px" src="icons/Technology/nextjs.svg" alt="next.svg" />
+                    </Box>
+                    <Box
+                        transition="all .2s ease-in-out"
+                        cursor={"pointer"}
+                        _hover={{
+                            transform: !isLowerThan768 ? "scale(1.2)" : "",
+                        }}
+                        minW="100px"
+                        minH="100px"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center">
                         <Image
                             objectFit="contain"
                             w="90px"
                             h="90px"
-                            src="icons/Technology/nextjs.svg"
-                            alt="React Native Developers.svg"
-                        />
-                    </Box>
-                    <Box minW="90px" minH="100px">
-                        <Image
-                            objectFit="contain"
-                            w="70px"
-                            h="70px"
                             src="icons/Technology/reactNative.svg"
-                            alt="React_Developers"
+                            alt="ReactNative"
                         />
                     </Box>
-                    <Box minW="90px" minH="100px">
-                        <Image
-                            objectFit="contain"
-                            w="90px"
-                            h="90px"
-                            src="icons/Technology/expo.svg"
-                            alt="React Native Developers.svg"
-                        />
+                    <Box
+                        transition="all .2s ease-in-out"
+                        cursor={"pointer"}
+                        _hover={{
+                            transform: !isLowerThan768 ? "scale(1.2)" : "",
+                        }}
+                        minW="100px"
+                        minH="100px"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center">
+                        <Image objectFit="contain" w="100px" h="100px" src="icons/Technology/expo.svg" alt="expo.svg" />
                     </Box>
-                    <Box minW="90px" minH="100px">
+                    <Box
+                        transition="all .2s ease-in-out"
+                        cursor={"pointer"}
+                        _hover={{
+                            transform: !isLowerThan768 ? "scale(1.2)" : "",
+                        }}
+                        minW="100px"
+                        minH="100px"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center">
                         <Image
                             objectFit="contain"
                             w="60px"
                             h="60px"
                             src="icons/Technology/storybook-icon.svg"
-                            alt="React_Developers"
+                            alt="Storybook"
                         />
                     </Box>
-                    <Box minW="90px" minH="100px">
+                    <Box
+                        transition="all .2s ease-in-out"
+                        cursor={"pointer"}
+                        _hover={{
+                            transform: !isLowerThan768 ? "scale(1.2)" : "",
+                        }}
+                        minW="100px"
+                        minH="100px"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center">
                         <Image
                             objectFit="contain"
                             w="60px"
                             h="60px"
                             src="icons/Technology/typescript-icon.svg"
-                            alt="React_Developers"
+                            alt="TypeScript"
                         />
                     </Box>
-                    <Box minW="90px" minH="100px">
+                    <Box
+                        transition="all .2s ease-in-out"
+                        cursor={"pointer"}
+                        _hover={{
+                            transform: !isLowerThan768 ? "scale(1.2)" : "",
+                        }}
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        minW="100px"
+                        minH="100px">
                         <Image
                             objectFit="contain"
                             w="60px"
                             h="60px"
                             src="icons/Technology/logo-javascript.svg"
-                            alt="React_Developers"
+                            alt="JavaScript"
                         />
                     </Box>
                 </Flex>
